71514f64fcf0fa6d408973f12e9491eafe69a373
ashutosh229 <akumarjha875@gmail.com>
2024-07-26 14:43:18 +0530

    files updated

---
+++ test.py
@@ -0,0 +1,11 @@
+from src.logger.logger import logging
+
+logging.info("this is myb testing")
+
+
+logging.info(" this my second tesgting")
+logging.info(" this my second tesgting")
+
+logging.info(" this my second tesgting")
+logging.info(" this my second tesgting")
+logging.info(" this my second tesgting")
